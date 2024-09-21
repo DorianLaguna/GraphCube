fb5dbe56d61977f0603cf379a133e1fbc941c4e2
DorianLaguna <richardlgcs@gmail.com>
2024-09-20 22:00:09 -0600

    agregar color a cada cubo y dividir el cubo en cubitos para poderlo rotar cada cubito

--- src/views/RenderModelView.vue
+++ src/views/RenderModelView.vue
@@ -6,6 +6,15 @@
   
   // Referencia al div donde se montará el renderizador
   const modelo = ref(null);
+  const colorMaterials = ref({
+    'yellow': new THREE.MeshBasicMaterial({ color: 0xffff00 }), // Amarillo
+    'green': new THREE.MeshBasicMaterial({ color: 0x00ff00 }),  // Verde
+    'blue': new THREE.MeshBasicMaterial({ color: 0x0000ff }),   // Azul Marino
+    'white': new THREE.MeshBasicMaterial({ color: 0xffffff }),  // Blanco
+    'orange': new THREE.MeshBasicMaterial({ color: 0xffa500 }), // Naranja
+    'red': new THREE.MeshBasicMaterial({ color: 0xff0000 }),    // Rojo
+    'black': new THREE.MeshBasicMaterial({ color: 0x000000 })   // Negro (para las caras internas)
+  });
   
   onMounted(() => {
     const scene = new THREE.Scene();
@@ -37,19 +46,31 @@
   
     // Luz direccional
     const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
-    directionalLight.position.set(5, 5, 5).normalize();
+    directionalLight.position.set(1, 1, 1).normalize();
     scene.add(directionalLight);
   
     const loader = new GLTFLoader();
   
     // Cargar modelo GLTF
-    loader.load('/models/cube.glb', function (gltf) {
-      const model = gltf.scene;
-      model.position.set(-1, -3, 0);
-      scene.add(model);
-      // console.log('Modelo cargado y añadido a la escena:', model); // Mensaje de consola para depuración
-    }, undefined, function (error) {
-      console.error(error);
+    const nameCubes = ["cube000","cube100","cube200","cube010", "cube210","cube220", "cube110","cube020","cube120","cube001","cube011","cube021","cube022","cube012","cube002","cube101","cube102","cube112","cube122","cube222","cube212","cube202","cube201","cube211","cube221","cube121"];
+    nameCubes.forEach(name => {
+      loader.load(`/models/${name}.glb`, function (gltf) {
+        const model = gltf.scene;
+        model.position.set(-1, -4, 1);
+        // Aplicar el material a cada malla del modelo
+        model.traverse((node) => {
+          console.log(node)
+          if (node.isMesh) { // Verificar si el nodo es una malla
+              if (colorMaterials.value[node.name]) {  // Verificar si el nombre del nodo tiene un color asociado
+                  node.material = colorMaterials.value[node.name]; // Asignar el material correspondiente
+              }
+          }
+      });
+        scene.add(model);
+        // console.log('Modelo cargado y añadido a la escena:', model); // Mensaje de consola para depuración
+      }, undefined, function (error) {
+        console.error(error);
+      });
     });
     
     const controls = new OrbitControls( camera, renderer.domElement );
@@ -63,7 +84,6 @@
       controls.update();
 
       renderer.render( scene, camera );
-
     }
     animate()
     // Ajustar el tamaño del renderizador al cambiar el tamaño de la ventana
